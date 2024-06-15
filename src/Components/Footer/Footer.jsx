3f2e519eaffd322a8cebdc55664ec9df33af5e3e
wawanmain22 <wawanmain22@gmail.com>
2024-06-16 02:14:23 +0700

    fix responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './Footer.css';
-import footer_logo from '../../assets/adit (4).png';
-import user_icon from '../../assets/user_icon.svg';
+import React from "react";
+import "./Footer.css";
+import footer_logo from "../../assets/adit (4).png";
+import user_icon from "../../assets/user_icon.svg";
 
 const Footer = () => {
   const handleSubscribeClick = () => {
@@ -9,7 +9,7 @@ const Footer = () => {
   };
 
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-top">
         <div className="footer-top-left">
           <img src={footer_logo} alt="Footer Logo" />
@@ -18,14 +18,18 @@ const Footer = () => {
         <div className="footer-top-right">
           <div className="footer-email-input">
             <img src={user_icon} alt="User Icon" />
-            <input type="email" placeholder='Enter Your Email' />
+            <input type="email" placeholder="Enter Your Email" />
           </div>
-          <div className="footer-subscribe" onClick={handleSubscribeClick}>Subscribe</div>
+          <div className="footer-subscribe" onClick={handleSubscribeClick}>
+            Subscribe
+          </div>
         </div>
       </div>
       <hr />
       <div className="footer-bottom">
-        <p className="footer-bottom-left">© 2024 Muhammad Aditya Pratama. All right reserved.</p>
+        <p className="footer-bottom-left">
+          © 2024 Muhammad Aditya Pratama. All right reserved.
+        </p>
         <div className="footer-bottom-right">
           <p>Terms of Services</p>
           <p>Privacy policy</p>
